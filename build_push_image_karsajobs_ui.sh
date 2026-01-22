d8fd018ffd44ba232debc67170ac45eddd4219ff
iqbal <ikbalganss20@gmail.com>
2026-01-22 09:09:16 +0700

    revisi lagi

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# 1. Definisikan variabel (ganti <username-docker> dengan username Anda)
+# 1. Definisi Variabel
 USER_DOCKER="ikbalbbm120"
 IMAGE_NAME="karsajobs-ui"
 
 # 2. Build Docker Image
-echo "Building Docker image for Backend..."
 docker build -t $USER_DOCKER/$IMAGE_NAME:latest .
 
 # 3. Login ke Docker Hub
-# Anda akan diminta memasukkan password setelah menjalankan ini
-echo "Logging in to Docker Hub..."
 docker login -u $USER_DOCKER
 
 # 4. Push Image ke Docker Hub
-echo "Pushing Backend image to Docker Hub..."
 docker push $USER_DOCKER/$IMAGE_NAME:latest
-
-echo "Done! Backend image has been pushed."
