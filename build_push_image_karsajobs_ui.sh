320a1737f9b73803cb2183e151525b4be004aca1
iqbal <ikbalganss20@gmail.com>
2026-01-22 09:00:15 +0700

    revisi

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# 1. Definisikan variabel secara terpisah agar lebih rapi
-REGISTRY="ghcr.io"
-USERNAME="ikbalbbm120"
+# 1. Definisikan variabel (ganti <username-docker> dengan username Anda)
+USER_DOCKER="ikbalbbm120"
 IMAGE_NAME="karsajobs-ui"
 
 # 2. Build Docker Image
-# Tag akan menjadi ghcr.io/ikbalbbm120/karsajobs-ui:latest
-echo "Building Docker image for Frontend..."
-docker build -t $REGISTRY/$USERNAME/$IMAGE_NAME:latest .
+echo "Building Docker image for Backend..."
+docker build -t $USER_DOCKER/$IMAGE_NAME:latest .
 
-# 3. Login ke GitHub Packages
-# Kita harus menyebutkan alamat registry-nya (ghcr.io)
-echo "Logging in to GitHub Packages..."
-docker login $REGISTRY -u $USERNAME
+# 3. Login ke Docker Hub
+# Anda akan diminta memasukkan password setelah menjalankan ini
+echo "Logging in to Docker Hub..."
+docker login -u $USER_DOCKER
 
-# 4. Push Image ke GitHub Packages
-echo "Pushing Frontend image to GitHub Packages..."
-docker push $REGISTRY/$USERNAME/$IMAGE_NAME:latest
+# 4. Push Image ke Docker Hub
+echo "Pushing Backend image to Docker Hub..."
+docker push $USER_DOCKER/$IMAGE_NAME:latest
 
-echo "Done! Frontend image has been pushed to GHCR."
+echo "Done! Backend image has been pushed."
